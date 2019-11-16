d790771c84668b72799718f83e476d6736ea348f
tserrien <thegeekest94@gmail.com>
2019-11-16 20:58:06 +0100

    sync

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+//TODO implement logger instead of printing to cl
+public class Main {
+
+    public static void main(String[] args) {
+        Path path;
+        String row;
+
+        try{
+            path = Paths.get(args[0]).toAbsolutePath();
+        }catch(Exception e){
+            path = Paths.get("C:\\Projects\\Java\\Benchmark\\src\\tester.csv");
+            e.printStackTrace(System.out);
+        }
+
+        String pathSt = path.toString();
+
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(pathSt));
+            while ((row = csvReader.readLine()) != null) {
+                String[] line = row.split(",");
+                for (int i = 0; i < line.length; i++) {
+                    System.out.print(line[i] + " ");
+                }
+                System.out.println();
+                // do something with the data
+            }
+            csvReader.close();
+        }catch (Exception e){
+            e.printStackTrace(System.out);
+        }
+    }
+}
